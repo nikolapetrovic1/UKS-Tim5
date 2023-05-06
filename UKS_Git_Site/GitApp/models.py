e5cf3d7e7583e400019b42b2ab21570aab7332df
marko.rakic98@gmail.com <marko.rakic98@gmail.com>
2023-05-06 18:56:16 +0200

    Started repository and user models

--- UKS_Git_Site/GitApp/models.py
+++ UKS_Git_Site/GitApp/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
 
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    role = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.username
+
+
 class Repository(models.Model):
     name = models.CharField(max_length=200)
+    contributors = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
