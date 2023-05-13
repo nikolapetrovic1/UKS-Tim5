bdd06680216ae2b48359ac7de7a6b8876e379d29
marko.rakic98@gmail.com <marko.rakic98@gmail.com>
2023-05-13 23:32:10 +0200

    Added database data

--- UKS_Git_Site/GitApp/management/commands/fill_database.py
+++ UKS_Git_Site/GitApp/management/commands/fill_database.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 
 from ...models import Repository
-
+from ...models import User
 
 class Command(BaseCommand):
     # args = '<args1 args2>'
@@ -10,14 +10,27 @@ class Command(BaseCommand):
     def _add_repositories(self):
         Repository.objects.all().delete()
 
-        r1 = Repository(id=1, name="Repo1")
+        r1 = Repository(id=1, name="Repo1",contributors="User1")
         r1.save()
 
-        r2 = Repository(id=2, name="Repo2")
+        r2 = Repository(id=2, name="Repo2",contributors="User2")
         r2.save()
 
-        r3 = Repository(id=3, name="Repo3")
+        r3 = Repository(id=3, name="Repo3",contributors="User3")
         r3.save()
-
+        
+    def _add_users(self):
+        User.objects.all().delete()
+        
+        u1 = User(id=1,name="User1")
+        u1.save()
+        
+        u2 = User(id=2,name="User2")
+        u2.save()
+        
+        u3 = User(id=3,name="User3")
+        u3.save()
+    
     def handle(self, *args, **options):
         self._add_repositories()
+        self._add_users()
