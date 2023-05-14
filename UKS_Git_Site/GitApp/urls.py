a12781e48a57c367cca4a585db132b5396ceb498
marko.rakic98@gmail.com <marko.rakic98@gmail.com>
2023-05-14 20:05:45 +0200

    Beginner version of adding users to repositories

--- UKS_Git_Site/GitApp/urls.py
+++ UKS_Git_Site/GitApp/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:repository_id>/", views.single_repo, name="single_repository"),
-    path("testredispage/", views.cached_initial, name="test_redis_page")
+    path("testredispage/", views.cached_initial, name="test_redis_page"),
+    path("<int:repository_id>/<int:user_id>/",views.add_users_to_repo, name="add_users_to_repo")
 ]
