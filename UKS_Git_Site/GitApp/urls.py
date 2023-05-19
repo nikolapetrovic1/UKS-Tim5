8000870b6c1ebd58710dc19ae80560ea40eeda30
Nikola Petrovic <s.nino.petrovic@gmail.com>
2023-05-19 15:00:06 +0200

    Password reset

--- UKS_Git_Site/GitApp/urls.py
+++ UKS_Git_Site/GitApp/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     path("user/delete/",user_view.delete_user, name="delete_user"),
     path("test/",user_view.test,name="test"),
     path("profile/",user_view.user_profile,name="user_profile"),
-    path("register/",user_view.user_register,name="user_register")
+    path("register/",user_view.user_register,name="user_register"),
+    # path("user/update",user_view.user_update,name="user_update"),
+    path("change_password",user_view.user_change_password,name="change_password")
+
+
 ]
--- UKS_Git_Site/GitApp/user_view.py
+++ UKS_Git_Site/GitApp/user_view.py
@@ -4,10 +4,32 @@ from django.contrib.auth.decorators import login_required, permission_required
 
 from django.core.exceptions import PermissionDenied
 from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 from .models import User
 
 
+class UserForm(UserCreationForm):
+    class Meta:
+        model = User     
+        # widgets = {
+        #     'name': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'roll': forms.NumberInput(attrs={'class': 'form-control'}),
+        # }
+        fields = ["first_name","last_name","email","username"]
+
+	# def save(self, commit=True):
+	# 	user = super(NewUserForm, self).save(commit=False)
+	# 	user.email = self.cleaned_data['email']
+	# 	if commit:
+	# 		user.save()
+	# 	return user
+
+
+class UserUpdateForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ["first_name","last_name","email","username"]
 
 @login_required()
 @permission_required('GitApp.test_access', raise_exception=True)
@@ -17,19 +39,14 @@ def test(request):
 @login_required()
 def user_profile(request):
     user = get_object_or_404(User, id=request.user.id)
-    return render(request,'user_profile.html',{"object": user})
+    form = UserUpdateForm(request.POST or None, instance=user) 
+    return render(request,'user_profile.html',{"object": user,"form": form})
 
 @login_required()
 def delete_user(request):
     user = get_object_or_404(User, id=request.user.id)
     user.delete()
     return redirect('index')
-# @login_required()
-    
-# class UserUpdate(UpdateView):
-#     model = User
-#     fields = ['name', 'pages']
-#     success_url = reverse_lazy('book_list')
 
 def user_login(request):
     if request.method == 'GET':
@@ -54,18 +71,7 @@ def user_logout(request):
     raise PermissionDenied()
 
 
-class UserForm(UserCreationForm):
-    class Meta:
-        model = User 
-        fields = ["first_name","last_name","username"]
-
-	# def save(self, commit=True):
-	# 	user = super(NewUserForm, self).save(commit=False)
-	# 	user.email = self.cleaned_data['email']
-	# 	if commit:
-	# 		user.save()
-	# 	return user
-
+       
 def user_register(request,template_name='user_form.html'):
     form = UserForm(request.POST or None)
     if form.is_valid():
@@ -76,8 +82,24 @@ def user_register(request,template_name='user_form.html'):
 login_required()
 def user_update(request, template_name='user_form.html'):
     user = get_object_or_404(User, pk=request.user.id)
-    form = UserForm(request.POST or None, instance=user)
+    form = UserUpdateForm(request.POST or None, instance=user)
     if form.is_valid():
         form.save()
         return redirect('user_profile')
     return render(request, template_name, {'form':form})
+
+@login_required()
+def user_change_password(request):
+    if request.method == 'POST':
+        
+        form = PasswordChangeForm(request.user,data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('user_profile')
+        else:
+            return redirect('change_password')
+    else:
+        form = PasswordChangeForm(user=request.user)
+        args = {'form': form}
+        return render(request, 'password_reset.html', args) 
