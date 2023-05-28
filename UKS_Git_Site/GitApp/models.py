7a9a47585617a1837bd5c61f2438d230b1ee5a14
Nikola Petrovic <s.nino.petrovic@gmail.com>
2023-05-28 14:05:21 +0200

    Mostly all classes and relationships added based on diagram

--- UKS_Git_Site/GitApp/models.py
+++ UKS_Git_Site/GitApp/models.py
@@ -22,29 +22,62 @@ class User(AbstractUser):
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
-    labels= models.ManyToManyField(Label)
+    labels = models.ManyToManyField(Label)
+    lead = models.ForeignKey(User)
+    developers = models.ManyToManyField(User,blank=True)
 
 class Repository(models.Model):
     name = models.CharField(max_length=200)
     contributors = models.CharField(
         max_length=200
     )  # to remove comment when data is added to database
-
+    
+    #TODO: project relation
     def __str__(self):
         return self.name
+class Branch(models.Model):
+    repository = models.ForeignKey(Repository,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
 
-
-
-class Event(models.Model):
+class Commit(models.Model):
     date_time: models.DateTimeField()
+    log_message: models.CharField()
+    hash: models.CharField()
+    #TODO: parents
+    author: models.ForeignKey(User)
+    commiter: models.ForeignKey(User)
+
+class Milestone(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    due_date = models.DateField()
+    state = models.CharField(max_length=2,choices=State.STATE_CHOICES,default=State.OPEN) 
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
 
 class Task(models.Model):
-    pass        
+    creator = models.ForeignKey(User)
+    assignees = models.ManyToManyField(User,blank=True)
+    milestone = models.ForeignKey(Milestone)
+class Event(models.Model):
+    date_time: models.DateTimeField()
+    task = models.ForeignKey(Task)
 
 class Comment(Event):
     content: models.TextField(max_length=1500)
     date_created: models.DateTimeField(auto_now=True)
     task: models.ForeignKey(Task,on_delete=models.CASCADE)
+
+class PullRequest(Task):
+    target = models.ForeignKey(Branch,on_delete=models.CASCADE)
+    source = models.ForeignKey(Branch,on_delete=models.CASCADE)
+    
+class Issue(Task):
+    title: models.CharField(max_length=200)
+    description: models.TextField()
+    date_created: models.DateField()
+    pull_request: models.ForeignKey(PullRequest)
+
 
 class StateChanged(Event):
     new_state: State
@@ -52,11 +85,5 @@ class StateChanged(Event):
 class LabelApplication(Event):
     label = models.ForeignKey(Label, on_delete=models.CASCADE)
 
-class Milestone(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.CharField(max_length=1000)
-    due_date = models.DateField()
-    #Note: enums cannot be used with ForeignKey
-    state = models.CharField(max_length=2,choices=State.STATE_CHOICES,default= State.OPEN) 
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
 
