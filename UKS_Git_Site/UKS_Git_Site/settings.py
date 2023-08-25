fd683adb2e39bf13d73eea7ec338b1ed7665cb43
Nikola Petrovic <40899687+nikolapetrovic1@users.noreply.github.com>
2023-08-25 12:19:53 +0200

    Update settings.py

--- UKS_Git_Site/UKS_Git_Site/settings.py
+++ UKS_Git_Site/UKS_Git_Site/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "crispy_forms",
-    "crispy_bootstrap4"
+    "crispy_bootstrap4",
     "polymorphic",
     "django_bootstrap_icons",
 ]
